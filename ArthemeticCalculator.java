065abdc0c2b84506424c46786242f4a647805d37
sravanchandra24 <164658695+sravanchandra24@users.noreply.github.com>
2024-09-02 11:01:38 +0530

    Update ArthemeticCalculator.java

--- ArthemeticCalculator.java
+++ ArthemeticCalculator.java
@@ -1,61 +1,63 @@
 package Programming;
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
 public class ArthemeticCalculator {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);	
-		double FirstNum,SecondNum,Result;
-		int num;
-		do {
-			System.out.println("CHOOSE ONE NUMBER: ");
-			System.out.println("1.ADD  2.SUB  3.MUL  4.DIV  5.EXIT");
-			num=sc.nextInt();
-			switch(num) {
-			case 1:
-				System.out.print("ENTER FIRST NUMBER : ");
-				FirstNum= sc.nextFloat();
-				System.out.print("ENTER SECOND NUMBER : ");
-				SecondNum= sc.nextFloat();
-				Result= FirstNum + SecondNum;
-				System.out.println("ADDITION OF " + FirstNum +" AND " + SecondNum + " IS : "+ Result);
-				System.out.println();
-				break;
-			case 2:
-				System.out.print("ENTER FIRST NUMBER : ");
-				FirstNum= sc.nextFloat();
-				System.out.print("ENTER SECOND NUMBER : ");
-				SecondNum= sc.nextFloat();
-				Result= FirstNum - SecondNum;
-				System.out.println("SUBTRACTION OF " + FirstNum +" AND " + SecondNum + " IS : "+ Result);
-				System.out.println();
-				break;
-			case 3:
-				System.out.print("ENTER FIRST NUMBER : ");
-				FirstNum= sc.nextFloat();
-				System.out.print("ENTER SECOND NUMBER : ");
-				SecondNum= sc.nextFloat();
-				Result= FirstNum * SecondNum;
-				System.out.println("MULTIPLICATION OF " + FirstNum +" AND " + SecondNum + " IS : "+ Result);
-				System.out.println();
-				break;
-			case 4:
-				System.out.println("ENTER FIRST NUMBER : ");
-				FirstNum= sc.nextFloat();
-				System.out.print("ENTER SECOND NUMBER : ");
-				SecondNum= sc.nextFloat();
-				Result= FirstNum / SecondNum;
-				System.out.println("DIVISION OF " + FirstNum +" AND " + SecondNum + " IS : "+ Result);
-				System.out.println(" ");				
-				break;
-			default :
-				if(num>5) {
-					System.out.println("---INVALID NUMBER---");
-				}
-				break;
-		  }
-		}while(num<5);
-		
-		sc.close();
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);    
+        double FirstNum, SecondNum, Result;
+        int num;
+
+        while (true) {
+            try {
+                System.out.println("CHOOSE ONE NUMBER: ");
+                System.out.println("1.ADD  2.SUB  3.MUL  4.DIV  5.EXIT");
+                num = sc.nextInt();
+
+                if (num == 5) {
+                    System.out.println("Exiting...");
+                    break;
+                }
+
+                if (num < 1 || num > 5) {
+                    System.out.println("---INVALID NUMBER---");
+                    continue;
+                }
+
+                System.out.print("ENTER FIRST NUMBER: ");
+                FirstNum = sc.nextDouble();
+                System.out.print("ENTER SECOND NUMBER: ");
+                SecondNum = sc.nextDouble();
+
+                switch (num) {
+                    case 1:
+                        Result = FirstNum + SecondNum;
+                        System.out.println("ADDITION OF " + FirstNum + " AND " + SecondNum + " IS : " + Result);
+                        break;
+                    case 2:
+                        Result = FirstNum - SecondNum;
+                        System.out.println("SUBTRACTION OF " + FirstNum + " AND " + SecondNum + " IS : " + Result);
+                        break;
+                    case 3:
+                        Result = FirstNum * SecondNum;
+                        System.out.println("MULTIPLICATION OF " + FirstNum + " AND " + SecondNum + " IS : " + Result);
+                        break;
+                    case 4:
+                        if (SecondNum == 0) {
+                            System.out.println("---DIVISION BY ZERO IS NOT ALLOWED---");
+                        } else {
+                            Result = FirstNum / SecondNum;
+                            System.out.println("DIVISION OF " + FirstNum + " AND " + SecondNum + " IS : " + Result);
+                        }
+                        break;
+                }
+                System.out.println();
+            } catch (InputMismatchException e) {
+                System.out.println("---INVALID INPUT, PLEASE ENTER NUMERIC VALUES ONLY---");
+                sc.next(); // Clear the invalid input
+            }
+        }
+
+        sc.close();
+    }
 }
-
-
